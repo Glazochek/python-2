984f54d00fd7e9f4ca6c1ffd2cee70e859aa8011
Glazochek <an9680411@gmail.com>
2025-11-11 18:04:44 +0100

    add trees in functions

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 n = int(input())
 
-for i in range(1, n + 1):
-    print(i * "*")
+
+def tree_1():
+    for i in range(1, n + 1):
+        print(i * "*")
+
+
 print("\n")
 
-for i in range(1, n + 1):
-    print(" ".join(i * ["*"]))
+
+def tree_2():
+    for i in range(1, n + 1):
+        print(" ".join(i * ["*"]))
+
+
 print("\n")
 
-for i in range(n, 0, -1):
-    print(" ".join(i * ["*"]))
+
+def tree_3():
+    for i in range(n, 0, -1):
+        print(" ".join(i * ["*"]))
+
+
 print("\n")
 
-for i in range(1, n + 1):
-    print(" " * ((n - i) * 2) + " ".join(i * ["*"]))
+
+def tree_4():
+    for i in range(1, n + 1):
+        print(" " * ((n - i) * 2) + " ".join(i * ["*"]))
+
+
 print("\n")
 
-for i in range(1, n + 1):
-    print(" " * (n - i) + " ".join(i * ["*"]))
-print("\n")
 
-for i in range(1, n + 1):
-    if i == n:
+def tree_5():
+    for i in range(1, n + 1):
         print(" " * (n - i) + " ".join(i * ["*"]))
-    else:
-        print(" " * (n - i) + " ".join(["*" if (o == 1 or o == i) else " " for o in range(1, i+1)]))
+
+
+print("\n")
+
+
+def tree_6():
+    for i in range(1, n + 1):
+        if i == n:
+            print(" " * (n - i) + " ".join(i * ["*"]))
+        else:
+            print(" " * (n - i) + " ".join(["*" if (o == 1 or o == i) else " " for o in range(1, i + 1)]))
+
+
 print("\n")
